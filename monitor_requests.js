0f8f85e08a8698435e8ca2dc51cb7bf5e7e4ebaa
ssdharma <ssdharma@ncsu.edu>
2016-11-05 17:59:36 -0400

    email configuration

--- monitor_requests.js
+++ monitor_requests.js
@@ -1,5 +1,8 @@
 var redis = require('redis');
 var ip = require('ip');
+var sendmail = require('sendmail')();
+
+
 var ipaddress = ip.address(); // REPLACE LATER on with the
 var client = redis.createClient(6379, '54.146.135.5', {});
 
@@ -12,10 +15,20 @@ ipaddress = args[0];
 
 setInterval(function() {
     client.get(ipaddress, function(error, reply) {
-        console.log("MNTR: " + reply.toString());
+
         if (parseInt(reply.toString()) > 4) { // threshold for requests per seconds
             // INSERT HERE logic for
+            console.log("MNTR: " + reply.toString());
             console.log("OVERLOAD");
+            sendmail({
+                from: 'no-reply@DevOps.com',
+                to: 'ssdharma@ncsu.edu, apatel10@ncsu.edu',
+                subject: 'REQUEST OVERLOAD at ' + ipaddress,
+                html: 'Server is overloading. Please check the server at ' + ipaddress
+            }, function(err, reply) {
+
+            });
+
         }
     });
 }, 200);
