dcca13149ba9cc4c434ce2142acb8b051f8ab3ac
ssdharma <ssdharma@ncsu.edu>
2016-11-08 01:26:31 -0500

    fixing commands

--- monitor_requests.js
+++ monitor_requests.js
@@ -8,42 +8,42 @@ var client = redis.createClient(6379, '52.90.252.26', {});
 
 // var args = process.argv.slice(2);
 // if (args.length == 0) {
-    // args = [ipaddress];
+// args = [ipaddress];
 // }
 // ipaddress = args[0];
 
 
 // setInterval(function() {
-    // client.get(ipaddress, function(error, reply) {
+// client.get(ipaddress, function(error, reply) {
 
-        // if (parseInt(reply.toString()) > 4) { // threshold for requests per seconds
-            // console.log("MNTR: " + reply.toString());
-            // console.log("OVERLOAD");
-            // client.lpop('active_servers', function(err, reply) {
-                // if (err) console.log(err);
-                // if (reply) {
-                    // client.lpush('serving_servers', reply, function(error, response) {
-                        // console.log('Provisioned new Server!');
-                    // });
-                // }
+// if (parseInt(reply.toString()) > 4) { // threshold for requests per seconds
+// console.log("MNTR: " + reply.toString());
+// console.log("OVERLOAD");
+// client.lpop('active_servers', function(err, reply) {
+// if (err) console.log(err);
+// if (reply) {
+// client.lpush('serving_servers', reply, function(error, response) {
+// console.log('Provisioned new Server!');
+// });
+// }
 
 
-            // });
-            // sendmail({
-                // from: 'no-reply@DevOps.com',
-                // to: 'ssdharma@ncsu.edu, apatel10@ncsu.edu',
-                // subject: 'REQUEST OVERLOAD at ' + ipaddress,
-                // html: 'Server is overloading. Please check the server at ' + ipaddress
-            // }, function(err, reply) {
+// });
+// sendmail({
+// from: 'no-reply@DevOps.com',
+// to: 'ssdharma@ncsu.edu, apatel10@ncsu.edu',
+// subject: 'REQUEST OVERLOAD at ' + ipaddress,
+// html: 'Server is overloading. Please check the server at ' + ipaddress
+// }, function(err, reply) {
 
-            // });
+// });
 
-        // }
-    // });
+// }
+// });
 // }, 200);
 
 exports.reqOverload = function(ip_addr) {
-    console.log("in hereee");
+
     client.lpop('active_servers', function(err, reply) {
         if (err) console.log(err);
         if (reply) {
@@ -51,17 +51,16 @@ exports.reqOverload = function(ip_addr) {
                 console.log('Provisioned new Server!');
             });
             sendDetails('Server is overloading. Provisioning new server was successful.\nOverloaded Server:', ip_addr);
-        }
-        else {
+        } else {
             sendDetails('Server is overloading. No new servers available to provision. Urgent attention required.\nOverloaded Server:', ip_addr)
         }
 
 
     });
-      console.log("HELLO");
+    console.log("HELLO");
 };
 
-function sendDetails(message, ip_addr){
+function sendDetails(message, ip_addr) {
     sendmail({
         from: 'no-reply@DevOps.com',
         to: 'ssdharma@ncsu.edu, apatel10@ncsu.edu',
--- stressmem.sh
+++ stressmem.sh
@@ -3,4 +3,4 @@
 echo "Stress Loading Memory"
 stress -m 1 --vm-bytes 256M -t 5s
 echo "Stress complete"
-sudo sysctl -w vm.drop_caches=3
+#sudo sysctl -w vm.drop_caches=3
