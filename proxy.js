6cd022df9b6363e300c7667a526b89621913e62e
Aniket Patel <ianiket18@gmail.com>
2016-11-08 12:56:06 -0500

    Adding canary to proxy

--- proxy.js
+++ proxy.js
@@ -1,15 +1,38 @@
 var redis = require('redis')
 var httpProxy = require('http-proxy')
 var http = require('http')
-var client = redis.createClient(6379, '54.146.135.5', {})
+var client = redis.createClient(6379, '52.90.252.26', {})
 
 
 var proxy = httpProxy.createProxyServer({});
 
+var counter = 0;
 var server = http.createServer(function(req, res){
-  client.rpoplpush('serving_servers', 'serving_servers', function(err, reply){
-    console.log('Request is being served by '+ reply);
-    proxy.web(req, res, {target: reply});
+  counter++;
+    if(counter < 4)
+    {
+        client.rpoplpush('serving_servers', 'serving_servers', function(err, reply){
+            console.log('Request is being served by '+ reply);
+            proxy.web(req, res, {target: reply});
+        });
+    }
+    else{
+        client.rpoplpush('canary_servers', 'canary_servers', function(err, reply){
+            if(err) throw err;
+            if(reply)
+            {
+                console.log('Request is being served by '+ reply);
+                proxy.web(req, res, {target: reply});
+            }
+            else{
+                client.rpoplpush('serving_servers', 'serving_servers', function(err, reply){
+                    console.log('Request is being served by '+ reply);
+                    proxy.web(req, res, {target: reply});
+                });
+            }
+        })
+        counter = 0;
+    }
   });
 });
 server.listen(3000);
