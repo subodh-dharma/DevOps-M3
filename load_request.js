19e30192081744edad7e8b6743f9cce9a7c7a26e
subodh-dharma <subodh.dharma@gmail.com>
2016-11-26 06:23:47 +0000

    adding new test urls

--- load_request.js
+++ load_request.js
@@ -5,5 +5,17 @@ var options = require('./test_urls.json');
 
 loadtest.loadTest(options[0], function(error, result)
 {
-    console.log('Load Test for root');
+    console.log('Load Test for /');
+});
+
+
+loadtest.loadTest(options[1], function(error, result)
+{
+    console.log('Load Test for /now');
+});
+
+
+loadtest.loadTest(options[2], function(error, result)
+{
+    console.log('Load Test for /noexist');
 });
