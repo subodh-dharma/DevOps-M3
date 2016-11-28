bd4e9ede873501827922a84dcae8b9515ab3829d
ssdharma <ssdharma@ncsu.edu>
2016-11-28 10:30:41 -0500

    updating monkey

--- load_request.js
+++ load_request.js
@@ -17,6 +17,12 @@ loadtest.loadTest(options[1], function(error, result)
 });
 
 
+loadtest.loadTest(options[2], function(error, result)
+{
+    if(error){ console.log("/now "+ error);}
+    console.log('Load Test for /now');
+});
+
 loadtest.loadTest(options[3], function(error, result)
 {
     if(error){ console.log("/ with headers "+ error);}
